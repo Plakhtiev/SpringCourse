19036d1d8653e3f5e49e6c2423cd7ef22c35035c
Jaroslav <monatkotata@gmail.com>
2022-07-08 14:32:39 +0300

    injected properties

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -8,6 +8,8 @@ public class Test3 {
 //        Pet pet = context.getBean("myPet", Pet.class);
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getAge());
+        System.out.println(person.getSurname());
 
         context.close();
     }
