42ab1e4defbe3d07474b6f89d25eeb3118ab8708
Jaroslav <monatkotata@gmail.com>
2022-07-09 10:57:12 +0300

    @Scope, @Autowired, @Component, Qualifier and more

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,5 +1,13 @@
 package spring_introduction;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+//@Scope("prototype")
 public class Dog implements Pet {
 //    private String name;
 
@@ -10,10 +18,11 @@ public class Dog implements Pet {
 //    public void setName(String name) {
 //        this.name = name;
 //    }
-
+@PostConstruct
     public void init() {
         System.out.println("Class Dog init method");
     }
+@PreDestroy
     public void destroy() {
         System.out.println("Class Dog destroy method");
     }
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,8 +1,19 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    @Autowired
+    @Qualifier("catBean")
     private Pet pet;
+    @Value("${person.surname}")
     private String surname;
+    @Value("${person.age}")
+    private int age;
 
     public void setSurname(String surname) {
         this.surname = surname;
@@ -20,16 +31,16 @@ public class Person {
         return age;
     }
 
-    private int age;
-
+//        @Autowired
 //    public Person(Pet pet) {
+//        System.out.println("Class Person is created");
 //        this.pet = pet;
 //    }
 
-    public void setPet(Pet pet) {
-        System.out.println("Class Person: set pet ");
-        this.pet = pet;
-    }
+//    public void setPet(Pet pet) {
+//        System.out.println("Class Person: set pet ");
+//        this.pet = pet;
+//    }
 
     public void callYourPet() {
         System.out.println("Hello, my lovely Pet!");
