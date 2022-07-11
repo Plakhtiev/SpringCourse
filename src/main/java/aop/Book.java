9fb965400356284c204ce2820b9f3d2f018ea1aa
DN210683PIN <yaroslav.plakhtiiev@privatbank.ua>
2022-07-11 17:08:29 +0300

    t

--- src/main/java/aop/Book.java
+++ src/main/java/aop/Book.java
@@ -7,6 +7,26 @@ import org.springframework.stereotype.Component;
 public class Book {
     @Value("Енеїда")
     public String nameBook;
+    @Value("Котляревський")
+    public String author;
+    @Value("1780")
+    public  int yearOfPublication;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
 
     public String getNameBook() {
         return nameBook;
--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -6,26 +6,32 @@ import org.springframework.stereotype.Component;
 public class UniLibrary extends AbstractLibrary {
     public void getBook() {
         System.out.println("We take a book from UniLibrary");
+        System.out.println("____________________________");
     }
 
     public void getMagazine() {
         System.out.println("We take a Magazine from UniLibrary ");
+        System.out.println("____________________________");
     }
 
     public void returnBook() {
         System.out.println("We return the book UniLibrary");
+        System.out.println("____________________________");
     }
 
     public void returnMagazine() {
         System.out.println("We return the Magazine into UniLibrary");
+        System.out.println("____________________________");
     }
 
-    public void addBook() {
+    public void addBook(String personName, Book book) {
         System.out.println("Add book to thr UniLibrary");
+        System.out.println("____________________________");
     }
 
     public void addMagazine() {
         System.out.println("Add Magazine to thr UniLibrary");
+        System.out.println("____________________________");
     }
 
 
