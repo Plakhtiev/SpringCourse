8675d0abfed069bcf95f9e449b583c9d8f77f6cc
Jaroslav <monatkotata@gmail.com>
2022-07-11 19:41:31 +0300

    Advice type AfterReturning

--- src/main/java/aop/aspect/MyPointCuts.java
+++ src/main/java/aop/aspect/MyPointCuts.java
@@ -3,6 +3,6 @@ package aop.aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class MyPointCuts {
-    @Pointcut("execution(* add*(..))")
+    @Pointcut("execution(* adc*(..))")
     public void allAddMethods(){}
 }
